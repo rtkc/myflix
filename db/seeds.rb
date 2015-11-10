643dfab4f1aba5bbebce801940d6b066b7b611b0
Ruby Tang <rubykct@gmail.com>
2015-11-10 17:23:42 +0800

    Show recent videos in specific category

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 
 # Using create! would be better than create as create! throws an exception when the record is not valid. Standard create does not do that. and in seeds.rb you want to see it because you run it from command line.
 Video.create!(title: "Monk", description: "Monk seed video", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create!(title: "Family Guy", description: "Family Guy seed video", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create!(title: "Futurama", description: "Futurama seed video", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create!(title: "Simpsons", description: "Simpsons seed video", small_cover_url: "/tmp/simpsons.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create!(title: "2 Broke Girls", description: "2 Broke Girls seed video", small_cover_url: "/tmp/2brokegirls.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create!(title: "South Park", description: "South Park seed video", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+
+
 Video.create!(title: "Monk", description: "Monk seed video", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 2)
 
 Category.create!(name: "Comedy")
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,44 @@ require 'spec_helper'
 
 describe Category do
   it { should have_many(:videos) }
+
+  describe "#recent_videos" do
+    it "Shows most recent videos in reverse chronological order" do
+      comedy = Category.create(name: "Comedy")
+      monk7 = Video.create(title: "Monk", description: "Monk test video 1", category: comedy, created_at: 7.day.ago)
+      monk6 = Video.create(title: "Monk 2", description: "Monk test video 2", category: comedy, created_at: 6.day.ago)
+      monk5 = Video.create(title: "Monk 3", description: "Monk test video 1", category: comedy, created_at: 5.day.ago)
+      monk4 = Video.create(title: "Monk 4", description: "Monk test video 2", category: comedy, created_at: 4.day.ago)
+      monk3 = Video.create(title: "Monk 5", description: "Monk test video 1", category: comedy, created_at: 3.day.ago)
+      monk2 = Video.create(title: "Monk 6", description: "Monk test video 2", category: comedy, created_at: 2.day.ago)
+      monk = Video.create(title: "Monk 7", description: "Monk test video 1", category: comedy, created_at: 1.day.ago)
+      expect(comedy.recent_videos).to eq([monk, monk2, monk3, monk4, monk5, monk6])
+    end
+
+    it "Shows 6 most recent videos in reverse chronological order" do
+      comedy = Category.create(name: "Comedy")
+      monk7 = Video.create(title: "Monk", description: "Monk test video 1", category: comedy, created_at: 7.day.ago)
+      monk6 = Video.create(title: "Monk 2", description: "Monk test video 2", category: comedy, created_at: 6.day.ago)
+      monk5 = Video.create(title: "Monk 3", description: "Monk test video 1", category: comedy, created_at: 5.day.ago)
+      monk4 = Video.create(title: "Monk 4", description: "Monk test video 2", category: comedy, created_at: 4.day.ago)
+      monk3 = Video.create(title: "Monk 5", description: "Monk test video 1", category: comedy, created_at: 3.day.ago)
+      monk2 = Video.create(title: "Monk 6", description: "Monk test video 2", category: comedy, created_at: 2.day.ago)
+      monk = Video.create(title: "Monk 7", description: "Monk test video 1", category: comedy, created_at: 1.day.ago)
+      expect(comedy.recent_videos).to eq([monk, monk2, monk3, monk4, monk5, monk6])
+    end
+
+    it "Shows all videos if less than 6 videos in the category" do
+      comedy = Category.create(name: "Comedy")
+      monk = Video.create(title: "Monk", description: "Monk test video 1", category: comedy, created_at: 1.day.ago)
+      monk2 = Video.create(title: "Monk 2", description: "Monk test video 2", category: comedy, created_at: 2.day.ago)
+      monk3 = Video.create(title: "Monk 3", description: "Monk test video 1", category: comedy, created_at: 3.day.ago)
+      expect(comedy.recent_videos).to eq([monk, monk2, monk3])
+    end
+
+    it "Returns empty array if no videos in category" do
+      comedy = Category.create(name: "Comedy")
+      horror = Category.create(name: "Horror")
+      expect(comedy.recent_videos).to eq([])
+    end
+  end
 end
