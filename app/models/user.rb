51fa1a4c853bf8011a0639dce5e4beac8f7f06b0
Ruby Tang <rubykct@gmail.com>
2015-11-19 10:14:30 +0800

    Update user model with has_many reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
+  
+  has_many :reviews
 
   validates :full_name, presence: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
