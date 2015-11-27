27daad4d2a89ad171297caf138ccf9a22d05298f
Ruby Tang <rubykct@gmail.com>
2015-11-27 14:40:33 +0800

    Create queue item model methods

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,16 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
   belongs_to :video, foreign_key: 'video_id', class_name: 'Video'
+
+  delegate :category, to: :video
+  delegate :title, to: :video, prefix: :video
+
+  def category_name
+    category.name
+  end
+
+  def rating
+    review = Review.where(user_id: user.id, video_id: video.id).first
+    review.rating if review
+  end
 end
