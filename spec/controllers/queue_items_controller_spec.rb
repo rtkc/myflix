7450bc94515e2f983332e34a3534476e1bf0d32f
Ruby Tang <rubykct@gmail.com>
2015-11-27 14:50:23 +0800

    Add create and destroy queue item tests

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -20,30 +20,44 @@ describe QueueItemsController do
     context "authenticated users" do
       let(:current_user) { Fabricate(:user) }
       before { session[:user_id] = current_user }
-      let(:video) { Fabricate(:video) }
 
-      it "sets @queue_item" do
-        post :create, video_id: video.id
-        expect(QueueItem.first.video).to eq(video)
-      end
-      it "sets @user" do
-        post :create, video_id: video.id
-        expect(QueueItem.first.user).to eq(current_user)
-      end
-      it "sets @video" do
-        post :create, video_id: video.id
-        expect(assigns(:video)).to eq(video)
-      end
       it "creates queue_item record" do
+        video = Fabricate(:video)
         post :create, video_id: video.id
         expect(QueueItem.count).to eq(1)
       end
       it "renders to video show template after saving queue_item record" do
+        video = Fabricate(:video)
         post :create, video_id: video.id
         response.should redirect_to(video_path(video))
       end
+      it "creates queue item associated with video" do
+        video = Fabricate(:video)
+        post :create, video_id: video.id
+        expect(QueueItem.first.video).to eq(video)
+      end
+      it "creates queue item associated with signed in user" do
+        video = Fabricate(:video)
+        post :create,  video_id: video.id
+        expect(QueueItem.first.user).to eq(current_user)
+      end
+      it "puts video as last one in queue" do
+        monk = Fabricate(:video)
+        post :create, video_id: monk.id
+        south_park = Fabricate(:video)
+        post :create, video_id: south_park.id
+        find_south_park_queue_item = QueueItem.where(video_id: south_park.id, user_id: current_user.id).first
+        expect(find_south_park_queue_item.order).to eq(2)
+      end
+      it "does not add video to queue if video already exists in queue" do
+        monk = Fabricate(:video)
+        post :create, video_id: monk.id
+        post :create, video_id: monk.id
+        count_monk_queue_item = QueueItem.where(video_id: monk.id, user_id: current_user.id).count
+        expect(count_monk_queue_item).to eq(1)
+      end
     end
-
+    
     it "redirects to sign in path for unauthenticted users" do
         video = Fabricate(:video)
         post :create, video_id: video.id
@@ -51,17 +65,47 @@ describe QueueItemsController do
       end
   end
 
-  describe "POST destroy" do
+  describe "DELETE destroy" do
     context "authenticated users" do
       let(:current_user) { Fabricate(:user) }
       before { session[:user_id] = current_user }
-      let(:video) { Fabricate(:video) }
+
+      it "destroys queue_item record" do
+        monk = Fabricate(:video)
+        monk_queue_item = QueueItem.create(video_id: monk.id, user_id: current_user.id)
+        delete :destroy, id: monk_queue_item.id
+        count_monk_queue_item = QueueItem.where(video_id: monk.id, user_id: current_user.id).count
+        expect(count_monk_queue_item). to eq(0)
+      end
+      it "redirects to new my queue page" do
+        monk = Fabricate(:video)
+        monk_queue_item = QueueItem.create(video_id: monk.id, user_id: current_user.id)
+        delete :destroy, id: monk_queue_item.id
+        count_monk_queue_item = QueueItem.where(video_id: monk.id, user_id: current_user.id).count
+        response.should redirect_to(queue_items_path) 
+      end
     end
-    it "destroys queue_item record" do
-      post :create, video_id: video.id
-      
-    end
-    it "renders new my queue template" do
+
+    context "unauthenticted users" do 
+      let(:alice) { Fabricate(:user) }
+      let(:bob) { Fabricate(:user) }
+      before { session[:user_id] = bob }
+
+      it "does not destroy queue_item record" do 
+        monk = Fabricate(:video)
+        monk_queue_item = QueueItem.create(video_id: monk.id, user_id: alice.id)
+        delete :destroy, id: monk_queue_item.id
+        count_monk_queue_item = QueueItem.where(video_id: monk.id, user_id: alice.id).count
+        expect(count_monk_queue_item). to eq(1)
+      end
+      it "redirects to sign in page" do 
+        monk = Fabricate(:video)
+        monk_queue_item = QueueItem.create(video_id: monk.id, user_id: alice.id)
+        delete :destroy, id: monk_queue_item.id
+        count_monk_queue_item = QueueItem.where(video_id: monk.id, user_id: alice.id).count
+        delete :destroy, id: monk_queue_item.id
+        response.should redirect_to(sign_in_path) 
+      end
     end
   end
 end
