f4799eb538aaa35c147c5b9367ead49e08de8df4
Ruby Tang <rubykct@gmail.com>
2015-11-27 15:06:11 +0800

    Revert "Change review to comment in reviews controller"
    This reverts commit 6095d0cbd3ca0cb71351745e0b02f7db12b5fe96.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ReviewsController < ApplicationController
 before_action :require_user
 
@@ -16,6 +17,6 @@ before_action :require_user
 
   private
   def review_params
-    params.require(:review).permit(:rating, :review)
+    params.require(:review).permit(:rating, :comment)
   end
 end
