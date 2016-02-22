aebd87bbd23ba578401866acb90a7044952afea9
Ruby Tang <rubykct@gmail.com>
2016-02-22 15:36:08 +0800

    Fix code to use fabricators to create queue items

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -8,25 +8,25 @@ describe QueueItem do
   describe "#video_title" do
     it "returns the title of the associated video" do
       monk = Fabricate(:video, title: 'Monk')
-      queue_item = QueueItem.create(video: monk)
+      queue_item = Fabricate(:queue_item, video: monk)
       expect(queue_item.video_title).to eq('Monk')
     end
   end
 
   describe "#category" do
     it "returns category name of associated video" do 
-      comedy = Category.create(name: 'Comedy')
+      comedy = Fabricate(:category)
       monk = Fabricate(:video, title: 'Monk', category: comedy)
-      queue_item = QueueItem.create(video: monk)
+      queue_item = Fabricate(:queue_item, video: monk)
       expect(queue_item.category).to eq(comedy)
     end
   end
 
   describe "#category_name" do
     it "returns category name of associated video" do 
-      comedy = Category.create(name: 'Comedy')
+      comedy = Fabricate(:category, name: 'Comedy')
       monk = Fabricate(:video, title: 'Monk', category: comedy)
-      queue_item = QueueItem.create(video: monk)
+      queue_item = Fabricate(:queue_item, video: monk)
       expect(queue_item.category_name).to eq('Comedy')
     end
   end
@@ -36,14 +36,14 @@ describe QueueItem do
       video = Fabricate(:video)
       user = Fabricate(:user)
       review = Fabricate(:review, creator: user, video: video, rating: 4)
-      queue_item = QueueItem.create(user: user, video: video)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
       expect(queue_item.rating).to eq(4)
     end
 
     it "returns nil if no review" do 
       video = Fabricate(:video)
       user = Fabricate(:user)
-      queue_item = QueueItem.create(user: user, video: video)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
       expect(queue_item.rating).to eq(nil)
     end
   end
